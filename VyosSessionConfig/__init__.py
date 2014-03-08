38b2f3fe17a15d583f5b2d8b5beee33748da03f9
mootez <mootez.bessifi@gmail.com>
2014-03-08 08:21:55 +0000

    add new VyosSessionConfig modifications to master

--- VyosSessionConfig/__init__.py
+++ VyosSessionConfig/__init__.py
@@ -1,36 +1,25 @@
 #!../bin/python
-
 import os
-import logging
 import ConfigParser
-#import ExecuterFormatter
 
-class sessionconfig:
 
-	shell_api_path = set_config_params('bin','shell_api_path')
+class sessionconfig():
 
-	def set_config_params(self,tag,key):
-		config = ConfigParser.SafeConfigParser()
-		config.readfp(open('../VyosApi.conf'))
-		return config.get(tag,key)
+        def set_config_params(self,tag,key):
+                config = ConfigParser.SafeConfigParser()
+                config.readfp(open('../VyosApi.conf'))
+                return config.get(tag,key)
 
-	def setup_config_session(self):
+        def setup_config_session(self,shell_api_path):
                 return ("""
-			session_env=$(%s getSessionEnv $PPID) 
-			eval $session_env 
-			%s setupSession
+                        session_env=$(%s getSessionEnv $PPID)
+                        eval $session_env
+                        %s setupSession
 
-			%s inSession
-			if [ $? -ne 0 ]; then
-        		echo "Something went wrong!"
-			fi
-		""" % (self.shell_api_path,self.shell_api_path,self.shell_api_path))
+                        %s inSession
+                        if [ $? -ne 0 ]; then
+                        echo "Something went wrong!"
+                        fi
+                """ % (shell_api_path,shell_api_path,shell_api_path))
 
-	def commit(self):
-                call([], name="commit")
 
-	def save(self):
-                call([], name="save")
-
-	def discard():
-		call([], name="discard")
