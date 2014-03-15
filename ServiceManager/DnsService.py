325963f91c7ee268e4f1ea41c65e7d2c309ba270
mootez <mootez.bessifi@gmail.com>
2014-03-15 16:41:19 +0000

    setup modifications on DnsService.py

--- ServiceManager/DnsService.py
+++ ServiceManager/DnsService.py
@@ -3,31 +3,40 @@
 import sys
 sys.path.append('../')
 from ConfigOpt import config_opt
-
+from ExecFormat import ExecutorFormator
 SDF="service dns forwarding"
 
 class dnsservice(config_opt):
-	dns_params = []
+	#exe = ExecutorFormator()
+
+	def dns_config(self,action,suffix):
+		dns_params=[SDF]
+		dns_params.extend(suffix)
+		if action == "set":
+			self.set(dns_params)
+		elif action == "delete":
+			self.delete(dns_params)
 
 	def add_listenon_interface(self,interface):
-		self.dns_params.extend((SDF,"listen-on",interface))
-		self.set(self.dns_params)
+		self.dns_config("set",["listen-on",interface])
 
 	def del_listenon_interface(self,interface):
-		self.dns_params.extend((SDF,"listen-on",interface))
-                self.delete(self.dns_params)
+		self.dns_config("delete",["listen-on",interface])
 
 	def add_nameserver(self,nameserver):
-		self.dns_params.extend((SDF,"name-server",nameserver))
-                self.set(self.dns_params)
+		self.dns_config("set",["name-server",nameserver])
+		#self.exe.commit()
 
 	def del_nameserver(self,nameserver):
-		self.dns_params.extend((SDF,"name-server",nameserver))
-                self.delete(self.dns_params)
-
+		self.dns_config("delete",["name-server",nameserver])
+		#self.exe.commit()
 				
 
 
-obj = dnsservice()
-obj.del_listenon_interface("eth0")
-#obj.add_nameserver("1.1.1.1")
+#obj = dnsservice()
+#obj.del_listenon_interface("eth0")
+#obj.del_nameserver("208.67.222.123")
+#obj.add_listenon_interface("eth0")
+#obj.add_listenon_interface("eth1")
+#obj.add_nameserver("208.67.222.123")
+
