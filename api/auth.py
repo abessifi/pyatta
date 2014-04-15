85f5acc86b837772bcae5222de17844c74a429ba
mootez <mootez.bessifi@gmail.com>
2014-04-15 16:41:47 +0000

    implement auth.py

--- api/auth.py
+++ api/auth.py
@@ -1,22 +1,49 @@
 #!/usr/bin/env python
+
 import os
-from flask import Flask, abort, request, jsonify, g, url_for, make_response
-from flask.ext.sqlalchemy import SQLAlchemy
+import sys
+topdir = os.path.dirname(os.path.realpath(__file__)) + "../.."
+topdir = os.path.realpath(topdir)
+sys.path.insert(0, topdir)
+from VyosSessionConfig import utils
+from flask import Flask, jsonify, abort, session, request, make_response, url_for,g
+from flask.views import MethodView
+from flask.ext.restful import Api, Resource, reqparse, fields, marshal
 from flask.ext.httpauth import HTTPBasicAuth
+from flask.ext.sqlalchemy import SQLAlchemy
+from sqlalchemy import update
 from passlib.apps import custom_app_context as pwd_context
 from itsdangerous import (TimedJSONWebSignatureSerializer
                           as Serializer, BadSignature, SignatureExpired)
-from pprint import pprint
 
-# initialization
-app = Flask(__name__)
-app.config['SECRET_KEY'] = '546f21a4eb62845c60fc'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////var/lib/pyatta/db.sqlite'
+#initialization
+app = Flask(__name__, static_url_path = "")
+app.config['SECRET_KEY'] = utils.get_config_params('api_auth','secret_key')
+app.config['SQLALCHEMY_DATABASE_URI'] = utils.get_config_params('api_auth','db_uri')
 app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
 
-# extensions
-db = SQLAlchemy(app)
+#extensions
+api = Api(app)
 auth = HTTPBasicAuth()
+db = SQLAlchemy(app)
+
+@app.route('/v1.0/token')
+@auth.login_required
+def get_auth_token():
+    token = g.user.generate_auth_token(60)
+    return jsonify({'token': token.decode('ascii'), 'duration': 600})
+
+@auth.error_handler
+def unauthorized():
+    return make_response(jsonify( { 'error': 'unauthorized access!','reason':User.error_msg } ), 401)
+    # return 403 instead of 401 to prevent browsers from displaying the default auth dialog
+
+user_fields = {
+    'username':fields.String,
+    'email':fields.String,
+    'superuser':fields.Boolean,
+    'uri':fields.Url('user')
+}
 
 
 class User(db.Model):
@@ -24,12 +51,12 @@ class User(db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(32), index=True)
-    password_hash = db.Column(db.String(64))
+    password = db.Column(db.String(64))
     email = db.Column(db.String(30))
     superuser=db.Column(db.Boolean, nullable=False)
-    
+
     def hash_password(self, password):
-        self.password_hash = pwd_context.encrypt(password)
+        self.password = pwd_context.encrypt(password)
 
     def set_email(self,email=""):
         self.email=email
@@ -38,7 +65,7 @@ class User(db.Model):
         self.superuser=su
 
     def verify_password(self, password):
-        return pwd_context.verify(password, self.password_hash)
+        return pwd_context.verify(password, self.password)
 
     def generate_auth_token(self, expiration=600):
         s = Serializer(app.config['SECRET_KEY'], expires_in=expiration)
@@ -56,102 +83,94 @@ class User(db.Model):
         user = User.query.get(data['id'])
         return user
 
-@app.errorhandler(404)
-def not_found(error):
-    return make_response(jsonify( { 'error': 'Not found' } ), 404)
-
-@app.errorhandler(405)
-def not_found(error):
-        return make_response(jsonify( { 'error': 'Request method not allowed!' } ), 405)
-
-@auth.error_handler
-def unauthorized():
-    return make_response(jsonify( { 'error': 'unauthorized access!','reason':User.error_msg } ), 401)
-    
-
 @auth.verify_password
 def verify_password(username_or_token, password):
     # first try to authenticate by token
     if password=='unused':
         user = User.verify_auth_token(username_or_token)
         if not user:
+            #return {'invalid token:':username_or_token}, 401
             User.error_msg="invalid token!"
             return False
         # try to authenticate with username/password
     else:
         user = User.query.filter_by(username=username_or_token).first()
         if not user or not user.verify_password(password):
+            #username_or_token+"/"+password
+            #return {'invalid provided username/password:':'failed'}, 401
             User.error_msg="username/password error!"
             return False
+        
     g.user = user
     return True
 
-def parse_json():
-    user = request.json.get('username')
-    pwd = request.json.get('password')
-    email=request.json.get('email')
-    su=request.json.get('superuser')
-    return user,pwd,email,su
+class UserListAPI(Resource):
+    decorators = [auth.login_required]
 
-@app.route('/v1.0/users', methods=['POST','GET'])
-def new_user():
-    if request.method=='POST':
-        username, password, email, superuser= parse_json()
-        if username is None or password is None or superuser is None or superuser not in [0,1]:
-            abort(400)    # missing arguments
-        if User.query.filter_by(username=username).first() is not None:
-            abort(400)    # existing user
-        user = User(username=username)
-        user.hash_password(password)
-        user.set_email(email)
-        user.set_superuser(superuser)
+    def __init__(self):
+        self.reqparse = reqparse.RequestParser()
+        self.reqparse.add_argument('username', type=str, required=True, help='No username provided', location='json')
+        self.reqparse.add_argument('password', type=str, required=True,help='No password provided', location='json')
+        self.reqparse.add_argument('email', type=str, default="", location='json')
+        self.reqparse.add_argument('superuser', type=int, required=True,help='No superuser privilege provided', location='json')
+        super(UserListAPI, self).__init__()
+        
+    def get(self):
+        return { 'users': map(lambda t: marshal(t, user_fields), User.query.all()) }
+
+    def post(self):
+        args = self.reqparse.parse_args()
+        if User.query.filter_by(username=args['username']).first() is not None:
+            return {'error':'username name already allocated!'}, 400
+        user = User(username=args['username'])
+        user.hash_password(args['password'])
+        user.set_email(args['email'])
+        user.set_superuser(args['superuser'])
         db.session.add(user)
         db.session.commit()
-        return (jsonify({'username': user.username}), 201,
-                {'Location': url_for('get_user', id=user.id, _external=True)})
-    elif request.method=='GET':
-        list_users= User.query.all()
-        for user in list_users:
-            print user.username,user.email
-        return jsonify({"done":"done"})
-    else:
-        abort(405)    
+        return { 'user': marshal(user, user_fields) }, 201
 
-@app.route('/v1.0/users/<int:id>', methods=['GET','DELETE','PUT'])
-def RUD_user(id):
-    user = User.query.get(id)
-    if not user:
-        abort(400)
-    if request.method=='GET':
-        return jsonify({'username': user.username,'email':user.email})
-    elif request.method=='DELETE':
-        delete_status=user.username+" has been deleted from the database!"
+class UserAPI(Resource):
+    decorators = [auth.login_required]
+
+    def __init__(self):
+        self.reqparse = reqparse.RequestParser()
+        self.reqparse.add_argument('username', type = str, location = 'json')
+        self.reqparse.add_argument('password', type = str, location = 'json')
+        self.reqparse.add_argument('email', type = str , location = 'json')
+        super(UserAPI, self).__init__()
+
+    def get(self, id):
+        user = User.query.get(id)
+        if not user:
+            abort(404)
+        return { 'user': marshal(user, user_fields) }
+
+    def put(self, id):
+        user = User.query.get(id)
+        if not user:
+            abort(404)
+        args = self.reqparse.parse_args()
+        if args['password']:
+            args['password']=pwd_context.encrypt(args['password'])
+        for cle in ['username','password','email']:
+            if args[cle]:
+                user=User.query.filter_by(id=id).update({cle:args[cle]})
+        db.session.commit()
+        #return {'done':'done'}, 200        
+        return { 'user': marshal(user, user_fields) }
+
+    def delete(self, id):
+        user = User.query.get(id)
+        if not user:
+            abort(404)
         db.session.delete(user)
         db.session.commit()
-        return jsonify({'Info':delete_status})
+        return { 'Info': 'mentioned user has been deleted successfully!'}
     
-    elif request.method=='PUT':
-        if username is None or password is None or superuser is None or superuser not in [0,1]:
-            abort(400)
-        #for elem in request.json.keys():
-         #   user.keys        
-    
-    else:
-        abort(405)
-
-@app.route('/api/token')
-@auth.login_required
-def get_auth_token():
-    token = g.user.generate_auth_token(60)
-    return jsonify({'token': token.decode('ascii'), 'duration': 600})
-
-@app.route('/api/resource')
-@auth.login_required
-def get_resource():
-    return jsonify({'data': 'Hello, %s!' % g.user.username})
-
+api.add_resource(UserListAPI, '/v1.0/users')
+api.add_resource(UserAPI, '/v1.0/users/<int:id>', endpoint = 'user')
 if __name__ == '__main__':
     if not os.path.exists('/var/lib/pyatta/db.sqlite'):
         db.create_all()
-    app.run(debug=True)
-
+    app.run(debug = True)
