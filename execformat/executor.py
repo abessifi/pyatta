a87a7224c2c3587362688ad6857d657853539800
Ahmed Bessifi <ahmed.bessifi@gmail.com>
2014-04-29 22:08:37 +0000

    refactor execformat/executor.py

--- execformat/executor.py
+++ execformat/executor.py
@@ -31,6 +31,19 @@ def check_operation_name(args):
         raise OperationNameError('Operation name not correct.')
     return True
 
+def _runner(command):
+    """
+    Run shell commands via subprocess.Popen()
+    """
+    # NOTE:
+    # if Popen(self.args, shell=True, ...) => Execution fails
+    # if Popen(self.args, ...) => OSError: [Errno 2] No such file or directory
+    # if self.args = ['/bin/cli-shell-api','showCfg', ...] and Popen(self.args, ...) that works but actually we keep using ' '.join(self.args).
+    proc = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    # wait for the process to terminate and get stdout/stderr outputs
+    out, err = proc.communicate()
+    return out, err, proc.returncode
+
 class execUtils:
     """
     Executes possible operations in a Vyos configure session.
@@ -49,26 +62,20 @@ class execUtils:
             if self.args[0] == 'show': self.args[0] = '{} showCfg'.format(VYOS_SHELL_API)
             else: self.args[0] = os.path.join(VYOS_SBIN_DIR, 'my_{}'.format(self.args[0]))
             logger.debug('exec command: "%s"' % ' '.join(self.args))
-            # NOTE:
-            # if Popen(self.args, shell=True, ...) => Execution fails
-            # if Popen(self.args, ...) => OSError: [Errno 2] No such file or directory
-            # if self.args = ['/bin/cli-shell-api','showCfg', ...] and Popen(self.args, ...) that works but actually we keep using ' '.join(self.args).
+            
             if not session.session_exists():
                 raise SessionNotExists('Configure session do not exists')
-            proc = subprocess.Popen(' '.join(self.args), shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-            # wait for the process to terminate and get stdout/stderr outputs
-            out, err = proc.communicate()
-            errcode = proc.returncode
+            result = _runner(' '.join(self.args)) # result = (stdout, stderr, errcode)
 
-            logger.debug('command return code: %s' % errcode)
-            
-            if errcode:
-                logger.info('command output: %s' % ' '.join(out.splitlines()))
+            logger.debug('command return code: %s' % result[2])
+
+            if result[2]:
+                logger.info('command output: %s' % ' '.join(result[0].splitlines()))
                 logger.error('Failed executing operation "%s"' % operation_name)
                 raise OperationFailed('Operation failed !')    
-            logger.debug('%s' % ' '.join(out.splitlines()))
+            logger.debug('%s' % ' '.join(result[0].splitlines()))
             logger.info('Executing "%s" operation OK' % operation_name)
-            return (True, out)
+            return (True, result[0])
 
     def check_cmd_args(self):
         """
@@ -79,11 +86,9 @@ class execUtils:
         logger.info('config path: "%s"' % config_path)
         cmd = '{} exists {}'.format(VYOS_SHELL_API, config_path)
         logger.debug('exec command: "%s"' % cmd)
-        proc = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        out, err = proc.communicate()
-        errcode = proc.returncode
-        logger.debug('command return code: %s' % errcode)
-        if errcode:
+        result = _runner(cmd) # result = (stdout, stderr, errcode)
+        logger.debug('command return code: %s' % result[2])
+        if result[2]:
             logger.error('Configuration path is not correct')
             raise ConfigPathNotCorrect('Configuration path is not correct')
         logger.info('Configuration path is correct')
@@ -103,13 +108,11 @@ class execUtils:
         logger.info('Get possible options of config path "%s"' % config_path)
         cmd = '{} listNodes {}'.format(VYOS_SHELL_API, config_path)
         logger.debug('exec command: "%s"' % cmd)
-        proc = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        out, err = proc.communicate()
-        errcode = proc.returncode
-        logger.debug('command return code: %s' % errcode)
-        if not out:
+        result = _runner(cmd) # rst = (stdout, stderr, errcode)
+        logger.debug('command return code: %s' % result[2])
+        if not result[0]:
             logger.info('No more options for the specified config path')
-            return True, out
-        options = shlex.split(out)
+            return True, result[0]
+        options = shlex.split(result[0])
         logger.debug('List of options : "%s"' % options)
         return True, options
